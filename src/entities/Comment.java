627a58bbe8f47ad8d56f43c8943d0eabed8486a8
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-28 12:52:57 -0300

    exercicio resolvido post

--- src/entities/Comment.java
+++ src/entities/Comment.java
@@ -1,5 +1,25 @@
 package entities;
 
 public class Comment {
-
+	//Atributos
+	private String text;
+	
+	// Método Getter e Setter
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	// Método Construtor
+	public Comment() {
+		
+	}
+	
+	public Comment(String text) {
+		this.text = text;
+	}
 }
